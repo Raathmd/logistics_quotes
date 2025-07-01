b0218d5a1428ebfafb6ad77b44c006c3713abfde
raathmd <raathmd@github.com>
2025-07-01 18:57:22 +0000

    add Ash domain configuration

--- config/config.exs
+++ config/config.exs
@@ -9,7 +9,8 @@ import Config
 
 config :logistics_quotes,
   ecto_repos: [LogisticsQuotes.Repo],
-  generators: [timestamp_type: :utc_datetime]
+  generators: [timestamp_type: :utc_datetime],
+  ash_domains: [LogisticsQuotes.Domain]
 
 # Configures the endpoint
 config :logistics_quotes, LogisticsQuotesWeb.Endpoint,
