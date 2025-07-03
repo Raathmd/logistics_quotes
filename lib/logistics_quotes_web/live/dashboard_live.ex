743e6efa09556540a678d3700ff8a985246bd64d
raathmd <raathmd@github.com>
2025-07-03 12:39:26 +0000

    fix quotes navigation link

--- lib/logistics_quotes_web/live/dashboard_live.ex
+++ lib/logistics_quotes_web/live/dashboard_live.ex
@@ -38,7 +38,7 @@ defmodule LogisticsQuotesWeb.DashboardLive do
             <.nav_link navigate={~p"/users"} active={@active_nav == "users"}>
               <Heroicons.users class="w-5 h-5 mr-3" /> Users
             </.nav_link>
-            <.nav_link navigate={~p"/quotes"} active={@active_nav == "quotes"}>
+            <.nav_link navigate={~p"/quotes/new"} active={@active_nav == "quotes"}>
               <.icon name="hero-document-text" class="w-5 h-5 mr-3" /> Quotes
             </.nav_link>
           </div>
